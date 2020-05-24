6509abcac6f71c656a9313087b89a6b7299227c5
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-24 23:09:43 +0530

    Create Area of Triangle.cpp

---
+++ Area of Triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace.std
+int main()
+{
+int b,h,area;
+cout<<"Enter base and height";
+cin>>b>>h;
+area=0.5(b*h);
+cout<<"Area of the triangle is"<<area;
+return 0;
+}
