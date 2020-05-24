568074c5204e3e15918e936d1c669c6f0ba0dc6c
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-24 23:17:50 +0530

    Create 7.Sum of n natural numbers.cpp

---
+++ 7.Sum of n natural numbers.cpp
@@ -0,0 +1,13 @@
+#Day1/Sum of n natural numbers
+
+#include<iostream>
+using namespace.std;
+int main()
+{
+int n;sum;
+cout<<"Enter the value of n";
+cin>>n;
+sum=n*(n+1)/2;
+cout<<"sum of n natural numbers is"<<sum;
+return 0;
+}
