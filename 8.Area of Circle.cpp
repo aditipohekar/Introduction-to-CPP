45c899a6aeeba3a2884854941d8167a65339ef7c
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-25 11:26:15 +0530

    Create 8.Area of Circle.cpp

---
+++ 8.Area of Circle.cpp
@@ -0,0 +1,13 @@
+#Day 2/Area of circle
+
+#include<iostream>
+using namespace std;
+int main()
+{
+float r,area;
+cout<<"Enter the radius";
+cin>>r;
+area=3.1425f*r*r;
+cout<<"area of circle is"<<area;
+return 0;
+}
