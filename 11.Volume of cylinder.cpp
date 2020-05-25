d574d04e1ad572492a450f8ae9913ceba99c3419
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-25 12:04:36 +0530

    Create 11.Volume of cylinder.cpp

---
+++ 11.Volume of cylinder.cpp
@@ -0,0 +1,14 @@
+#Day 2/Volume of cylinder
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+float r,h,vol;
+cout<<"enter radius and height of cylinder";
+cin>>r>>h;
+vol=3.145*r*r*h;
+cout<<"vol. is"<<vol<<endl;
+return 0;
+}
