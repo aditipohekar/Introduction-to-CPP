6aadc71ffeb760afc6878f35479033203719d3b3
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-25 15:35:30 +0530

    Create 16.Enum.cpp

---
+++ 16.Enum.cpp
@@ -0,0 +1,13 @@
+#Day 2/Enum
+
+#include<iostream>
+using namespace std;
+enum day {mon,tue,wed,thurs,fri,sat,sun};
+int main()
+{
+day d;
+d=mon;           #you can assign the value mentioned in here
+cout<<d<<endl;
+return0;
+}
+
