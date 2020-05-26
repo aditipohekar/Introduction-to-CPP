27fe6fddb54e0a71d4a97c9ad4861c98687ec328
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-26 11:10:31 +0530

    Create 2.Positive or Negative?.cpp

---
+++ 2.Conditional Statements/2.Positive or Negative?.cpp
@@ -0,0 +1,19 @@
+#Day 3/If-else/Positive or Negative
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"Enter the number";
+cin>>n;
+if(n>0)
+{
+cout<<"Number is positive";
+}
+else
+{
+cout<<"Number is negative";
+}
+return 0;
+}
