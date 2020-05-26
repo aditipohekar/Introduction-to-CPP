b785ae896a2d50423a7174653c7088919283629e
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-26 11:13:55 +0530

    Create 3.Even or Odd?.cpp

---
+++ 2.Conditional Statements/3.Even or Odd?.cpp
@@ -0,0 +1,19 @@
+#Day 3/If-else/Even or Odd?
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"Enter the number";
+cin>>n;
+if(n/2==0)
+{
+cout<<"The number is even";
+}
+else
+{
+cout<<"The number is odd";
+}
+return 0;
+}
