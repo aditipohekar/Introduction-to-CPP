75b0922904632858c7ae0f472f2b982f0054add8
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-26 11:06:47 +0530

    Create 1.If-else.cpp

---
+++ 2.Conditional Statements/1.If-else.cpp
@@ -0,0 +1,19 @@
+#Day 3/If-else/Maximum of 2 nos.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"Enter the two numbers";
+cin>>a>>b;
+if(a>b)
+{
+cout<<"Max is"<<" "<<a;
+}
+else
+{
+cout<<"Max is"<<" "<<b;
+}
+return 0;
+}
