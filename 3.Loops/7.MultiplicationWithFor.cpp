c429e5e7831c76686a3e26eaa7644b99adce24c2
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-28 20:47:12 +0530

    Create 7.MultiplicationWithFor.cpp

---
+++ 3.Loops/7.MultiplicationWithFor.cpp
@@ -0,0 +1,18 @@
+#Day 4
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i,N;
+  cout<<"Enter the number";
+  cin>>n;
+  for(i=1;i<=10;i++)
+  {
+  N=n*i;
+ cout<<N<<endl;
+    
+  }
+  return 0;
+}
