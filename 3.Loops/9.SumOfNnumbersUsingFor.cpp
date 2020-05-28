b75724cc7ec148f4cfa1b2798a822d72a49f788a
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-28 20:57:34 +0530

    Create 9.SumOfNnumbersUsingFor.cpp

---
+++ 3.Loops/9.SumOfNnumbersUsingFor.cpp
@@ -0,0 +1,17 @@
+#Day 4
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i,Sum=0;
+  cout<<"Enter the number";
+  cin>>n;
+  for(i=1;i<=n;i++)
+  {
+    Sum=Sum+i;
+    cout<<"Sum ="<<Sum<<endl;
+    
+  }
+  return 0;
+}
