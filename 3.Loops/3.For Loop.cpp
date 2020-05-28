fced8b34561cb71f6c0f3f6961da41fcf55ead83
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-28 15:32:36 +0530

    Create 3.For Loop.cpp

---
+++ 3.Loops/3.For Loop.cpp
@@ -0,0 +1,15 @@
+#Day 4/For Loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i;
+  cout<<"Enter the number";
+  cin>>n;
+ for(i=1;i<=n;i++)
+  {
+    cout<<i<<endl;
+  }
+  return 0;
+}
