0b445f50f8c21531f177bfc1db9a13c7f2a9c92e
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-28 20:51:03 +0530

    Create MultiplicationTableWithFor.cpp

---
+++ 3.Loops/MultiplicationTableWithFor.cpp
@@ -0,0 +1,16 @@
+#Day 4
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i,N;
+  cout<<"Enter the number";
+  cin>>n;
+  for(i=1;i<=10;i++)
+  {
+ cout<<n<<""<<"X"<<i<<"="<<i*n<<endl;
+    
+  }
+  return 0;
+}
