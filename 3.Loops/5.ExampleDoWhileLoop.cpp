c15861610eb6e496f0de0823e4350f18bdfc480e
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-28 15:46:45 +0530

    Create 5.ExampleDoWhileLoop.cpp

---
+++ 3.Loops/5.ExampleDoWhileLoop.cpp
@@ -0,0 +1,18 @@
+#Day 4
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i=1;
+  cout<<"Enter the number";
+  cin>>n;
+  do
+  {
+    cout<<"hello"<<endl;
+    i++;
+  }
+  while(i<=n);
+  return 0;
+}
