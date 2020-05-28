cf67b3d700d2da8dfba8af7b0adb5d893775160f
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-28 15:50:30 +0530

    Create 6.ExampleForLoop.cpp

---
+++ 3.Loops/6.ExampleForLoop.cpp
@@ -0,0 +1,16 @@
+#Day 4
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i;
+  cout<<"Enter the number";
+  cin>>n;
+  for(i=1;i<=n;i++)
+  {
+    cout<<"hello"<<endl;
+    
+  }
+  return 0;
+}
