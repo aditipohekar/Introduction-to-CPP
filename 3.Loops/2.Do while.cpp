aa31059927ca14bcefff6457ac7b9f7799b06ee5
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-05-28 15:21:41 +0530

    Create 2.Do while.cpp

---
+++ 3.Loops/2.Do while.cpp
@@ -0,0 +1,17 @@
+#Day 4/Do while
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,i=1;
+  cout<<"Enter the number";
+  cin>>n;
+ do
+  {
+    cout<<i<<endl;
+    i++;
+  }
+   while(i<=n);
+  return 0;
+}
