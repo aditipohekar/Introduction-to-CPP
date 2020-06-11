3ead62edebf18fe57e479938b82b7b17c8e61d63
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-06-11 07:51:14 +0530

    Create 22.Example.cpp

---
+++ 6.String/22.Example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main() {
+
+  char s1[20]="PROGRAMMING";
+  char s2[20]="PROGRAMMINg";
+  cout<<strcmp(s1,s2)<<endl;
+  return 0;
+}
