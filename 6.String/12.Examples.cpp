739ff42f978dbb94c06ac92593c6ff9783dec90b
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-06-11 07:15:02 +0530

    Create 12.Examples.cpp

---
+++ 6.String/12.Examples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main() {
+  char *s;
+
+  cout <<"Enter a String"<<endl;
+  cin>>s; 
+  cout<<"Length"<<strlen(s)<<endl;
+  
+  return 0;
+}
