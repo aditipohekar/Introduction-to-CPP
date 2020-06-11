a4f625c09acaf7fd7f40d309d473ae5939880387
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-06-11 21:53:11 +0530

    Create 36.Class String Swap.cpp

---
+++ 6.String/36.Class String Swap.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  string str1 ="AditiPohekar";
+  string str2 ="Language";
+  str1.swap(str2);
+  cout<<str1<<endl;
+  cout<<str2<<endl;
+  return 0;
+}
